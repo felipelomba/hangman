47b425e8f9b19e5d3d0732c43adad7249678f2ae
Felipe Lomba <felipe.rlomba@gmail.com>
2018-12-28 13:00:19 -0200

    bug fixed in try again

--- lib/try_again.rb
+++ lib/try_again.rb
@@ -5,7 +5,7 @@
     def trying
         puts 'Wanna try again?'
         button = gets.to_str
-        if button == "s"
+        if button.chomp == "s"
             system('ruby app.rb')
         else
             return 'See you again!'
