cacb9ddfa69a766436269b9b57a46f117259623c
Felipe Lomba <felipe.rlomba@gmail.com>
2019-01-07 15:45:00 -0200

    refatoring new game feature

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
-require_relative 'lib/hangman' 
+require_relative 'lib/new_game' 
 require_relative 'lib/try_again'
 
-game = Hangman.new
- 
-    puts "Guess the Word"
-    word = gets.to_str
-    puts game.TryToGuess(word)
+game = NewGame.new
+
+game.starting_game
+
+
     
